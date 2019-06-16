80338ada7967cc48a7451c5b82cc3b1e4f98a84c
Dahan Schuster <dan.plschuster@gmail.com>
2019-06-16 15:11:40 -0300

    Nada importante, só testes

--- public_html/udemy-php7/mysqli/mysqli::query.php
+++ public_html/udemy-php7/mysqli/mysqli::query.php
@@ -5,7 +5,7 @@
 $servidor = "localhost";
 $usuario = "root";
 $senha = "root";
-$banco_dados = "dbphp7";
+$banco_dados = "Produtos";
 
 $conexao = new mysqli($servidor, $usuario, $senha, $banco_dados);
 
@@ -13,14 +13,17 @@ $conexao = new mysqli($servidor, $usuario, $senha, $banco_dados);
 /* REALIZANDO QUERYS COM A FUNÇÃO mysqli::query */
 
 # retorna um objeto do tipo mysqli_result e atribui à variável $resultado
-$resultado = $conexao->query("SELECT * FROM tb_usuarios ORDER BY login");
+$resultado = $conexao->query("SELECT * FROM usuarios");
 
 # o objeto mysqli_result $resultado possui o método fetch_array que percorre o resultado da query
 # como a query foi do tipo consulta (SELECT ... FROM ...), o resultado será um array com as linhas
 # (rows) que se encaixam na consulta.
 $rows = array();
 while ($row = $resultado->fetch_array()) {
-    array_push($rows, $row);
+    #array_push($rows, $row);
+    if ($row['login'] == 'admin' && $row['senha'] == '1234') {
+    	echo 'yeah!';
+    }
 }
 
-echo json_encode($rows);
+#echo json_encode($rows);
